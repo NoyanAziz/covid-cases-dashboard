a2bf07f44514193b5019224b2957227dc6f3498e
NoyanAziz <noyanaziz@A006-00629.local>
2021-12-09 01:54:44 +0500

    Custom command created to fetch data from covid cases urls

---
+++ covid_dashboard/covid_cases_app/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class State(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class CountryProvince(models.Model):
+    class Meta:
+        unique_together = ["province_name", "country"]
+    province_name = models.CharField(max_length=100)
+
+    country = models.ForeignKey(Country, related_name="provinces", on_delete=models.CASCADE)
+
+
+class GlobalCovidCase(models.Model):
+    class Meta:
+        unique_together = ["date", "country_province"]
+
+    date = models.DateField()
+    confirmed = models.PositiveIntegerField()
+    deaths = models.PositiveIntegerField()
+    recovered = models.PositiveIntegerField()
+
+    country_province = models.ForeignKey(CountryProvince, related_name="covid_cases", on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, related_name="covid_cases", on_delete=models.CASCADE)
+
+
+class USCovidCase(models.Model):
+    class Meta:
+        unique_together = ["date", "state"]
+
+    date = models.DateField()
+    confirmed = models.PositiveIntegerField()
+    deaths = models.PositiveIntegerField()
+
+    state = models.ForeignKey(State, related_name="covid_cases", on_delete=models.CASCADE)
