edcdfc80e04b8cbfad3fe47694084a26d749ae9e
NoyanAziz <noyanaziz@A006-00629.local>
2021-12-09 02:57:10 +0500

    Switched to postgres

--- covid_dashboard/covid_dashboard/settings.py
+++ covid_dashboard/covid_dashboard/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'covid_dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'covid_cases',
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
